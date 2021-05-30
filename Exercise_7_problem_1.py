b9f70f2fcc2109e0d7fcc61c4db2cfa002b14eee
kbc713 <>
2021-05-30 15:53:15 +0000

    I created a variable outputfp and save the plot.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -58,9 +58,14 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 
 # Plot a scatter plot
 # YOUR CODE HERE 3
-
+import matplotlib.pyplot as plt
+plt.scatter(x,y,s=50,c='black',cmap='rainbow',edgecolor='red')
 
 # Add labels and title
+plt.title("My random candy points")
+plt.xlabel("X-labels")
+plt.ylabel("y.labels")
+plt.show()
 # YOUR CODE HERE 4
 
 # Save the plot as a png file:
